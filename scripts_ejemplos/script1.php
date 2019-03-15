e54b530f90d6c9b8cb21c8335dfd528c60cf9978
santiaguin3 <dacer_12@hotmail.com>
2019-03-15 12:01:56 -0500

    Scripts php

---
+++ scripts_ejemplos/script1.php
@@ -0,0 +1,15 @@
+ <br>
+<center>Programacion avanzada</center>
+ <br>
+     <?php
+     $nombre="Peter";
+     $apellido="Parker";
+     $edad=30;
+     $salario=800;
+     echo"<center>Bienvenidos";
+     echo "Nombre: ".$nombre ."<br>";
+     echo "Apellido: ".$apellido;
+     echo "<br>Edad: ".$edad;
+     echo "<br>Salario ".$salario;
+     ?>
+<center>Fin del programa</center>
---
+++ scripts_ejemplos/script2.php
@@ -0,0 +1,17 @@
+<?php
+
+  $num1=10;
+  $num2=5;
+//Primer calculadora
+//  $suma=$num1 + $num2;
+  $resta=$num1 - $num2;
+  $mult=$num1 * $num2;
+  $div=$num1 / $num2;
+  echo "<center>";
+  echo "La suma es: ". ($num1 + $num2);
+  echo "<br>La resta es: ". $resta;
+  echo "<br>La multiplicacion es: ". $mult;
+  echo "<br>La divicion es: ". $div;
+  echo "</center>";
+
+ ?>
